64f3cc3607477332325fcd5e6fa56c41019ecd07
root2raj <ruturaj1999@gmail.com>
2021-02-24 23:19:16 +0530

    Revert "whyred: Add protobuf vendorcompat lib"
    This reverts commit d3e06cec8c62e6a30db2b832ca83b2f408b8cca4.

--- device.mk
+++ device.mk
@@ -386,11 +386,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
